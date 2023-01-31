4296fe5b568df27748104c963f256fcc135246b0
x1337loser <78683355+x1337loser@users.noreply.github.com>
2023-02-01 01:03:43 +0600

    Update npm-automation.sh
    silly mistake on path

--- npm-automation.sh
+++ npm-automation.sh
@@ -5,7 +5,7 @@ echo '''
   /  |/ / __ \/ __ `__ \   / /| |/ / / / __/ __ \
  / /|  / /_/ / / / / / /  / ___ / /_/ / /_/ /_/ /
 /_/ |_/ .___/_/ /_/ /_/  /_/  |_\__,_/\__/\____/ 
-     /_/                                           v1.0.0
+     /_/                                           v1.0.1
 
               twitter.com/@0xnirob        
 
@@ -19,9 +19,9 @@ else
     mkdir $PWD/$1;
 fi
 echo -e "Running waybackurls on $1"
-waybackurls $1 | sort -u | grep .js | sed 's/?.*//' | grep -v '/wp-content/\|/wp-includes/\|.json\|jpg\|png\|css|\|/member/\|.jsp\|oauth\|login\|en-us\|v=\|=\|?\|/help/\|/id/\|paragon\|/wp-json/' | sort -u | tee -a $HOME/$1/$1-js-urls.txt >/dev/null 2>&1;
+waybackurls $1 | sort -u | grep .js | sed 's/?.*//' | grep -v '/wp-content/\|/wp-includes/\|.json\|jpg\|png\|css|\|/member/\|.jsp\|oauth\|login\|en-us\|v=\|=\|?\|/help/\|/id/\|paragon\|/wp-json/' | sort -u | tee -a $PWD/$1/$1-js-urls.txt >/dev/null 2>&1;
 echo -e "Running gau on $1"
-gau $1 | sort -u | grep .js | sed 's/?.*//' | grep -v '/wp-content/\|/wp-includes/\|.json\|jpg\|png\|css|\|/member/\|.jsp\|oauth\|login\|en-us\|v=\|=\|?\|/help/\|/id/\|paragon\|/wp-json/' | sort -u | tee -a $HOME/$1/$1-js-urls.txt >/dev/null 2>&1;
+gau $1 | sort -u | grep .js | sed 's/?.*//' | grep -v '/wp-content/\|/wp-includes/\|.json\|jpg\|png\|css|\|/member/\|.jsp\|oauth\|login\|en-us\|v=\|=\|?\|/help/\|/id/\|paragon\|/wp-json/' | sort -u | tee -a $PWD/$1/$1-js-urls.txt >/dev/null 2>&1;
 
 cd $PWD/$1;
 echo -e "Found $(cat $1-js-urls.txt | sort -u |wc -l) js file url ";
@@ -41,7 +41,7 @@ if [ -s $1-npm-packages.txt ];then
             echo -e ""$ut" \e[1;31mFound Private npm packgae, \e[0m" && echo $ut >> $1-npm-vuln.txt;
 
         else
-            echo -e ""$ut" Availible in Public Registry ";
+            echo -e ""$ut"\e[1;33m Available in Public Registry \e[0m";
         fi
         done
 else
@@ -56,12 +56,12 @@ if [ -s $1-npm-scope.txt ];then
     cat $1-npm-scope.txt | sort -u | while read pkg;do
     OPTION=`curl -o /dev/null -s -w "%{http_code}\n" "https://www.npmjs.com/org/$pkg"`
         if $(echo "$OPTION" | grep "200\|302" >/dev/null 2>&1);then
-            echo -e ""$pkg" Availible in Public Registry " && echo $pkg >> $1-npm-scope-vuln.txt;
+            echo -e "@"$pkg"\e[1;33m Available in Public Registry \e[0m" && echo $pkg >> $1-npm-scope-vuln.txt;
         elif $(echo "$OPTION" | grep "429" >/dev/null 2>&1);then
-            echo -e ""$pkg" \e[1;31m Rate limit detected \e[0m"
+            echo -e "@"$pkg" \e[1;31m Rate limit detected \e[0m"
 
         else
-            echo -e ""$pkg"\e[1;31m Found Unclaimed scope Name\e[0m";
+            echo -e "@"$pkg"\e[1;31m Found Unclaimed scope Name\e[0m";
         fi
         done
 else
