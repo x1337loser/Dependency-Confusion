d88b5180e703bf3e20ce27a30722142478cd1527
x1337loser <78683355+x1337loser@users.noreply.github.com>
2024-02-10 17:57:07 +0600

    Update npm-automation.sh
    Fixed grammar issue and added feature to print the full URL of unclaimed npm package and scope name

--- npm-automation.sh
+++ npm-automation.sh
@@ -35,16 +35,17 @@ if [ -s $1-npm-packages.txt ];then
     echo -e "   Found some packages now going for final test on "$1-npm-packages.txt"";
     cat $1-npm-packages.txt | sort -u | while read ut;do
         if $(curl -o /dev/null -s -w "%{http_code}\n" "https://registry.npmjs.org/$ut" | grep "404" >/dev/null 2>&1); then
-            echo -e ""$ut" \e[1;31mFound Private npm packgae, \e[0m" && echo $ut >> $1-npm-vuln.txt;
+            echo -e ""$ut" \e[1;31mFound Private npm package, \e[0m" && echo $ut >> $1-npm-vuln.txt;
+            echo -e "\e[1;31mprivate package URL: https://registry.npmjs.org/$ut \e[0m"
 
         else
             echo -e ""$ut"\e[1;33m Available in Public Registry \e[0m";
         fi
         done
 else
-    echo -e "Didn't found any npm packages, now going for scope test "
+    echo -e "No npm packages found, now going for scope test "
 fi
-#this part is for scope package test please be carefull with that, some times `www.npmjs.com` will show you 429 response code
+#this part is for the scope package test please be careful with that, some times `www.npmjs.com` will show you 429 response code
 grep -oriahE "[^\"\\'> ]+" | grep 'node_modules' | sed 's:.*/node_modules::' | cut -d '/' -f 2 | sort -u | grep '@' | grep -v '.js\|.ts\|.tsx\|.css' | egrep '\b[a-z]+\b' | grep -v '.png\|.pnp' | grep '@' | cut -d '@' -f 2 | tee -a $1-npm-scope.txt >/dev/null 2>&1;
 
 if [ -s $1-npm-scope.txt ];then
@@ -53,16 +54,17 @@ if [ -s $1-npm-scope.txt ];then
     OPTION=`curl -o /dev/null -s -w "%{http_code}\n" "https://www.npmjs.com/org/$pkg"`
         if $(echo "$OPTION" | grep "200\|302" >/dev/null 2>&1);then
             echo -e "@"$pkg"\e[1;33m Available in Public Registry \e[0m" && echo $pkg >> $1-npm-scope-vuln.txt;
-            grep -oriahE "[^\"\\'> ]+" | grep 'node_modules' |grep '@'$pkg'' | sed 's:.*/@'$pkg'::' | cut -d '/' -f 2 | sort -u | while read ut;do echo "Full pacakge name of @"$pkg" is @"$pkg"/"$ut" ";done
+            grep -oriahE "[^\"\\'> ]+" | grep 'node_modules' |grep '@'$pkg'' | sed 's:.*/@'$pkg'::' | cut -d '/' -f 2 | sort -u | while read ut;do echo "Full package name of @"$pkg" is @"$pkg"/"$ut" ";done
         elif $(echo "$OPTION" | grep "429" >/dev/null 2>&1);then
             echo -e "@"$pkg" \e[1;31m Rate limit detected \e[0m"
 
         else
             echo -e "@"$pkg"\e[1;31m Found Unclaimed scope Name\e[0m";
-            grep -oriahE "[^\"\\'> ]+" | grep 'node_modules' |grep '@'$pkg'' | sed 's:.*/@'$pkg'::' | cut -d '/' -f 2 | sort -u | while read ut;do echo -e "\e[1;31mFull pacakge name of @"$pkg" is @"$pkg"/"$ut", this is unclaimed, Add @"$pkg"/"$ut" in your package.json file like {package: @"$pkg"/"$ut"}, \e[0m";done
+            echo -e "\e[1;31mprivate org URL: https://www.npmjs.com/org/$pkg \e[0m"
+            grep -oriahE "[^\"\\'> ]+" | grep 'node_modules' |grep '@'$pkg'' | sed 's:.*/@'$pkg'::' | cut -d '/' -f 2 | sort -u | while read ut;do echo -e "\e[1;31mFull package name of @"$pkg" is @"$pkg"/"$ut", this is unclaimed, Add @"$pkg"/"$ut" in your package.json file like {package: @"$pkg"/"$ut"}, \e[0m";done
         fi
         done
 else
-    echo -e "Didn't found any Scope name";
+    echo -e "No Scope name found";
 fi
 rm $1-npm-scope.txt *.map.* *.map $1-npm-packages.txt;
